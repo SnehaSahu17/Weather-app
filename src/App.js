e208b0101d65c9e53e74cc7c43eaf4c70de4d891
Sneha Sahu <snehasahu@Snehas-MacBook-Air.local>
2023-03-01 19:14:11 +0530

    ft:layout designed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import "./App.css";
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import TopButtons from "./components/TopButtons";
+import Inputs from "./components/Inputs";
+import TimeandLocation from "./components/TimeandLocation";
+import TemperatureandDetails from "./components/TemperatureandDetails";
+import Forecast from "./components/Forecast";
 function App() {
-  return <div className="App">
-    hello</div>;
+  return (
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 shadow-xl shadow-grap-400 h-fit">
+      <TopButtons/>
+      <Inputs/>
+      <TimeandLocation/>
+      <TemperatureandDetails/>
+      <Forecast title="Hourly Forecast"/>
+      <Forecast title="Hourly Forecast"/>
+    </div>
+  )  
 }
-
 export default App;
---
+++ src/components/TimeandLocation.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function TimeandLocation() {
+  return (
+    <div>
+        <div className="flex items-center justify-center my-6">
+        <p className="text-white text-xl font-extralight">
+          ........................
+        </p>
+      </div>
+
+      <div className="flex items-center justify-center my-3">
+        <p className="text-white text-3xl font-medium">Berlin,USA</p>
+      </div>
+    </div>
+  )
+}
+
+export default TimeandLocation
