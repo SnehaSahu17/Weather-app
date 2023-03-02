5ea7eaf13cde16ca0cff751aa808aeb27a0e4fec
Sneha Sahu <snehasahu@Snehas-MacBook-Air.local>
2023-03-02 08:44:17 +0530

    ft:features added

--- src/components/TemperatureandDetails.jsx
+++ src/components/TemperatureandDetails.jsx
@@ -1,37 +1,52 @@
-import React from 'react'
+import React from "react";
 import {
-    UilTemperature,
-    UilTear,
-    UilWind,
-    UilSun,
-    UilSunset,
-  } from "@iconscout/react-unicons";
+  UilTemperature,
+  UilTear,
+  UilWind,
+  UilSun,
+  UilSunset,
+} from "@iconscout/react-unicons";
+import { formatToLocalTime, iconUrlFromCode } from "../services/WeatherService";
 
-function TemperatureandDetails() {
+function TemperatureAndDetails({
+  weather: {
+    details,
+    icon,
+    temp,
+    temp_min,
+    temp_max,
+    sunrise,
+    sunset,
+    speed,
+    humidity,
+    feels_like,
+    timezone,
+  },
+}) {
   return (
     <div>
-        <div className="flex items-center justify-center py-6 text-xl text-cyan-300">
-        <p>Clear</p>
+      <div className="flex items-center justify-center py-6 text-xl text-cyan-300">
+        <p>{details}</p>
       </div>
 
       <div className="flex flex-row items-center justify-between text-white py-3">
-        <img src="https://res.cloudinary.com/dkqxnquga/image/upload/v1671958702/images_n49uxa.jpg" alt="" className="w-20" />
-        <p className="text-5xl">36°</p>
+        <img src={iconUrlFromCode(icon)} alt="" className="w-20" />
+        <p className="text-5xl">{`${temp.toFixed()}°`}</p>
         <div className="flex flex-col space-y-2">
           <div className="flex font-light text-sm items-center justify-center">
             <UilTemperature size={18} className="mr-1" />
             Real fell:
-            <span className="font-medium ml-1">32°</span>
+            <span className="font-medium ml-1">{`${feels_like.toFixed()}°`}</span>
           </div>
           <div className="flex font-light text-sm items-center justify-center">
             <UilTear size={18} className="mr-1" />
             Humidity:
-            <span className="font-medium ml-1">65%</span>
+            <span className="font-medium ml-1">{`${humidity.toFixed()}%`}</span>
           </div>
           <div className="flex font-light text-sm items-center justify-center">
             <UilWind size={18} className="mr-1" />
             Wind:
-            <span className="font-medium ml-1">11 km/hr</span>
+            <span className="font-medium ml-1">{`${speed.toFixed()} km/h`}</span>
           </div>
         </div>
       </div>
@@ -41,7 +56,7 @@ function TemperatureandDetails() {
         <p className="font-light">
           Rise:{" "}
           <span className="font-medium ml-1">
-            08:45 PM
+            {formatToLocalTime(sunrise, timezone, "hh:mm a")}
           </span>
         </p>
         <p className="font-light">|</p>
@@ -50,7 +65,7 @@ function TemperatureandDetails() {
         <p className="font-light">
           Set:{" "}
           <span className="font-medium ml-1">
-            04:34 Am
+            {formatToLocalTime(sunset, timezone, "hh:mm a")}
           </span>
         </p>
         <p className="font-light">|</p>
@@ -58,18 +73,18 @@ function TemperatureandDetails() {
         <UilSun />
         <p className="font-light">
           High:{" "}
-          <span className="font-medium ml-1">40°</span>
+          <span className="font-medium ml-1">{`${temp_max.toFixed()}°`}</span>
         </p>
         <p className="font-light">|</p>
 
         <UilSun />
         <p className="font-light">
           Low:{" "}
-          <span className="font-medium ml-1">19°</span>
+          <span className="font-medium ml-1">{`${temp_min.toFixed()}°`}</span>
         </p>
       </div>
     </div>
-  )
+  );
 }
 
-export default TemperatureandDetails
+export default TemperatureAndDetails;
--- src/components/TimeandLocation.jsx
+++ src/components/TimeandLocation.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from "react";
+import { formatToLocalTime } from "../services/WeatherService";
 
-function TimeandLocation() {
+function TimeAndLocation({ weather: { dt, timezone, name, country } }) {
   return (
     <div>
-        <div className="flex items-center justify-center my-6">
+      <div className="flex items-center justify-center my-6">
         <p className="text-white text-xl font-extralight">
-          ........................
+          {formatToLocalTime(dt, timezone)}
         </p>
       </div>
 
       <div className="flex items-center justify-center my-3">
-        <p className="text-white text-3xl font-medium">Berlin,USA</p>
+        <p className="text-white text-3xl font-medium">{`${name}, ${country}`}</p>
       </div>
     </div>
-  )
+  );
 }
 
-export default TimeandLocation
+export default TimeAndLocation;
